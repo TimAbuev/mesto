1bb7113cbce30ed4623165d6613efe75ecd5c88f
Tim <you@example.com>
2022-07-15 00:43:44 +0300

    добавить событие для closeIcon

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,12 @@
 const buttonEdit = document.querySelector('.profile__button-edit');
+const popupBlock = document.querySelector('.popup');
 
 buttonEdit.addEventListener('click', function(){
-  const popupBlock = document.querySelector('.popup');
   popupBlock.className = 'popup popup_opened';
 });
+
+const closeIcon = document.querySelector('.popup__closeIcon');
+closeIcon.addEventListener('click', function(){
+  popupBlock.className = 'popup';
+});
+
