0b94cb2b913af56eed9927e9fc3b0158ce779698
Tim <you@example.com>
2022-07-15 16:57:41 +0300

    разработка buttonSave

--- scripts/index.js
+++ scripts/index.js
@@ -10,3 +10,16 @@ closeIcon.addEventListener('click', function(){
   popupBlock.className = 'popup';
 });
 
+
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__busy');
+const popupName = document.querySelector('.popup__name');
+const popupJob = document.querySelector('.popup__working');
+
+const buttonSave = document.querySelector('.popup__save');
+buttonSave.addEventListener('click', function(){
+  profileName.textContent = 's oshta';
+  popupBlock.className = 'popup';
+});
+
+
