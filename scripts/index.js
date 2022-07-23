8ef0def8d0abcb4dc8677eba18831775d36f1209
Tim <you@example.com>
2022-07-23 04:19:13 +0300

    добавить модификаторы к инпутам

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
 const popupBlock = document.querySelector('.popup');
 const buttonEdit = document.querySelector('.profile__button-edit');
-const closeIcon = document.querySelector('.popup__closeIcon');
+const closeIcon = document.querySelector('.popup__close-icon');
 const formPopup = document.querySelector('.popup__form');
 const profileName = document.querySelector('.profile__name');
-const popupName = document.querySelector('.popup__name');
+const popupName = document.querySelector('.popup__input_type_name');
 const profileJob = document.querySelector('.profile__busy');
-const popupJob = document.querySelector('.popup__working');
+const popupJob = document.querySelector('.popup__input_type_job');
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
@@ -22,6 +22,8 @@ function formCloseHandler () {
   popupBlock.className = 'popup';
 }
 
-closeIcon.addEventListener('click', formCloseHandler);
 formPopup.addEventListener('submit', formSubmitHandler);
 buttonEdit.addEventListener('click', formEditHandler);
+closeIcon.addEventListener('click', formCloseHandler);
+
+
