26e800f2f4dcd5d469028c794ddb865e7e32c9bc
Tim <you@example.com>
2022-07-29 16:51:47 +0300

    добавить 6 cards from the box js

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,34 @@ const profileName = document.querySelector('.profile__name');
 const popupName = document.querySelector('.popup__input_type_name');
 const profileJob = document.querySelector('.profile__busy');
 const popupJob = document.querySelector('.popup__input_type_job');
+const elementsImage = document.querySelectorAll('.elements__image');
+const elementsTitle = document.querySelectorAll('.elements__title');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
 function closePopup() {
   popupBlock.className = 'popup';
@@ -15,7 +43,7 @@ function formSubmitHandler (evt) {
   profileName.textContent = popupName.value;
   profileJob.textContent = popupJob.value;
   closePopup();
-}
+} 
 function formEditHandler () {
   popupBlock.className = 'popup popup_opened'; 
   popupName.value = profileName.textContent;
@@ -26,4 +54,9 @@ formPopup.addEventListener('submit', formSubmitHandler);
 buttonEdit.addEventListener('click', formEditHandler);
 closeIcon.addEventListener('click', closePopup);
 
-
+elementsTitle.forEach(function(item, index) { 
+  item.textContent = initialCards[index].name;
+});
+elementsImage.forEach(function(item, index) {
+  item.src = initialCards[index].link;
+});
