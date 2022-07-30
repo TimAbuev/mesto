e147d9f2d277d1bc56c43cb9cea90c8c3a5f31c3
Tim <you@example.com>
2022-07-31 01:19:02 +0300

    добавить скрипт добавления карточки

--- scripts/popup-mesto.js
+++ scripts/popup-mesto.js
@@ -1,65 +1,95 @@
-const profileButton = document.querySelector('.profile__button');
-const popupMestoBlock = document.querySelector('.popup-mesto');
-const closeIconMesto = document.querySelector('.popup-mesto__close-icon');
-const buttonMestoSave = document.querySelector('.popup-mesto__save');
-const elementsImage = document.querySelectorAll('.elements__image');
-const elementsTitle = document.querySelectorAll('.elements__title');
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-const formMesto = document.querySelector('.popup-mesto__form');
-const templateCardContent = document.querySelector('#template-card').content;
-const templateCardContentClone = templateCardContent.querySelector('.elements__card').cloneNode(true);
-const elements = document.querySelector('.elements');
-//elements.prepend(templateCardContentClone);
-
-function openFormMesto() {
-  popupMestoBlock.className = 'popup-mesto popup-mesto_opened';
-}
-function closeFormMesto() {
-  popupMestoBlock.className = 'popup-mesto';
-}
-//function addCard() {   }
-function SubmitHandlerFormMesto(event) {
-  event.preventDefault();
-  elements.prepend(templateCardContentClone);
-  closeFormMesto();
+const selectors = {
+  form: '.popup-mesto__form',
+  inputName: '.popup-mesto__input_type_name',
+  inputLink: '.popup-mesto__input_type_link',
+  list: '.elements',
+  buttonPlus: '.profile__button',
+  blockPopup: '.popup-mesto',
+  buttonClose: '.popup-mesto__close-icon',
 }
 
-elementsTitle.forEach(function (item, index) {
-  item.textContent = initialCards[index].name;
-});
-elementsImage.forEach(function (item, index) {
-  item.src = initialCards[index].link;
-});
+const form = document.querySelector(selectors.form);
+const inputName = form.querySelector(selectors.inputName);
+const inputLink = form.querySelector(selectors.inputLink);
+const list = document.querySelector(selectors.list);
+const buttonPlus = document.querySelector(selectors.buttonPlus);
+const blockPopup = document.querySelector(selectors.blockPopup);
+const buttonClose = document.querySelector(selectors.buttonClose);
 
-formMesto.addEventListener('submit', SubmitHandlerFormMesto);
-profileButton.addEventListener('click', openFormMesto);
-closeIconMesto.addEventListener('click', closeFormMesto);
-//buttonMestoSave.addEventListener('click', addCard);
+function addCard(name, src) {
+  const template = `    <div class="elements__card">
+  <img src="${name.link}" alt="" class="elements__image">
+  <button class="elements__trash"></button>
+  <div class="elements__wrapper">
+    <h2 class="elements__title">${name.name}</h2>
+    <button class="elements__like" type="button"></button>
+  </div>
+</div>`;
 
+  list.insertAdjacentHTML('afterbegin', template);
+}
+function addNewCard(name, src) {
+  const template = `    <div class="elements__card">
+  <img src="${src}" alt="" class="elements__image">
+  <button class="elements__trash"></button>
+  <div class="elements__wrapper">
+    <h2 class="elements__title">${name}</h2>
+    <button class="elements__like" type="button"></button>
+  </div>
+</div>`;
+
+  list.insertAdjacentHTML('afterbegin', template);
+}
+
+function closePopup() {
+  blockPopup.className = 'popup-mesto';
+}
+
+function addEventListeners() {
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    addNewCard(inputName.value, inputLink.value);
+    closePopup();
+  })
+  buttonPlus.addEventListener('click', function() {
+    blockPopup.className = 'popup-mesto popup-mesto_opened';
+  })
+  buttonClose.addEventListener('click', function() {
+    closePopup();
+  })
+}
+
+function createInitialCards() {
+  const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+  initialCards.forEach((item) => addCard(item));
+}
+
+addEventListeners();
+createInitialCards();
 
 
