63ca48153d0a2c5e4e4c65e2dae4fa64378b30a9
TimAbuev <tim-r10@yandex.ru>
2022-08-03 02:17:45 +0300

    вынести лишние константы из функций в global

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,80 +1,42 @@
-const selectors = {
-  form: '.popup-mesto__form',
-  inputName: '.popup-mesto__input_type_name',
-  inputLink: '.popup-mesto__input_type_link',
-  list: '.elements',
-  buttonPlus: '.profile__button',
-  blockPopup: '.popup-mesto',
-  buttonClose: '.popup-mesto__close-icon',
-  template: '.template-card',
-  divElementsCard: '.elements__card',
-  elementsTitle: '.elements__title',
-  elementsImage: '.elements__image',
-  trashButton: '.elements__trash',
-  wrapperButton: '.elements__button-wrapper',
-  blockPopupImage: '.popup-image',
-  buttonToCloseImg: '.popup-image__close-icon',
-  popopImageImage: '.popup-image__image',
-  caption: '.popup-image__caption',
-  like: '.elements__like',
-
-}
-const form = document.querySelector(selectors.form);
-const inputName = form.querySelector(selectors.inputName);
-const inputLink = form.querySelector(selectors.inputLink);
-const list = document.querySelector(selectors.list);
-const buttonPlus = document.querySelector(selectors.buttonPlus);
-const blockPopup = document.querySelector(selectors.blockPopup);
-const buttonClose = document.querySelector(selectors.buttonClose);
-const blockPopupImage = document.querySelector(selectors.blockPopupImage);
-const buttonToCloseImg = blockPopupImage.querySelector(selectors.buttonToCloseImg);
-const popopImageImage = blockPopupImage.querySelector(selectors.popopImageImage);
-const caption = blockPopupImage.querySelector(selectors.caption);
-
 function addCard(p1) {
-  const template = document.querySelector(selectors.template).content.querySelector(selectors.divElementsCard).cloneNode(true);
-  const elementsTitle = template.querySelector(selectors.elementsTitle);
+  const newTemplate = template.cloneNode(true);
+  const elementsTitle = newTemplate.querySelector(selectors.elementsTitle);
   elementsTitle.textContent = p1.name;
-  template.querySelector(selectors.elementsImage).setAttribute('src', p1.link);
-  template.querySelector(selectors.elementsImage).setAttribute('alt', elementsTitle.textContent);
+  newTemplate.querySelector(selectors.elementsImage).setAttribute('src', p1.link);
+  newTemplate.querySelector(selectors.elementsImage).setAttribute('alt', elementsTitle.textContent);
 
-  template.querySelector(selectors.trashButton).addEventListener('click', function() {template.remove();})
-  template.querySelector(selectors.wrapperButton).addEventListener('click', function() {
-    blockPopupImage.className = 'popup-image popup-image_opened';
+  newTemplate.querySelector(selectors.trashButton).addEventListener('click', function() {template.remove();})
+  newTemplate.querySelector(selectors.wrapperButton).addEventListener('click', function() {
+    popupImage.className = 'popup-image popup-image_opened';
     popopImageImage.setAttribute('src', p1.link);
     caption.textContent = p1.name;
   });
-  template.querySelector(selectors.like).addEventListener('click', function() {
-    template.querySelector(selectors.like).classList.toggle('elements__like_active');
+  newTemplate.querySelector(selectors.like).addEventListener('click', function() {
+    newTemplate.querySelector(selectors.like).classList.toggle('elements__like_active');
   });
 
-  list.appendChild(template);
+  list.appendChild(newTemplate);
 }
 
 function addNewCard(name, src) {
-  const template = document.querySelector(selectors.template).content.querySelector(selectors.divElementsCard).cloneNode(true);
-  const  elementsTitle = template.querySelector(selectors.elementsTitle);
+  const newTemplate = template.cloneNode(true);
+  const  elementsTitle = newTemplate.querySelector(selectors.elementsTitle);
   elementsTitle.textContent = name;
-  template.querySelector(selectors.elementsImage).setAttribute('src', src);
-  template.querySelector(selectors.elementsImage).setAttribute('alt', elementsTitle.textContent);
+  newTemplate.querySelector(selectors.elementsImage).setAttribute('src', src);
+  newTemplate.querySelector(selectors.elementsImage).setAttribute('alt', elementsTitle.textContent);
 
-  template.querySelector(selectors.trashButton).addEventListener('click', function() {template.remove();})
-  template.querySelector(selectors.wrapperButton).addEventListener('click', function() {
-    blockPopupImage.className = 'popup-image popup-image_opened';
+  newTemplate.querySelector(selectors.trashButton).addEventListener('click', function() {newTemplate.remove();})
+  newTemplate.querySelector(selectors.wrapperButton).addEventListener('click', function() {
+    popupImage.className = 'popup-image popup-image_opened';
     popopImageImage.setAttribute('src', src);
     caption.textContent = name;
   })
-  template.querySelector(selectors.like).addEventListener('click', function() {
-    template.querySelector(selectors.like).classList.toggle('elements__like_active');
+  newTemplate.querySelector(selectors.like).addEventListener('click', function() {
+    newTemplate.querySelector(selectors.like).classList.toggle('elements__like_active');
   });
 
-  list.insertBefore(template, list.firstChild);
+  list.insertBefore(newTemplate, list.firstChild);
 }
-
-// function closePopup(p1, p2) {
-//   p1.className = 'popup-mesto';
-//   p2.className = 'popup-image';
-// }
 
 function addEventListeners() {
   form.addEventListener('submit', function(event) {
@@ -83,14 +45,12 @@ function addEventListeners() {
     closePopup(popup);
   })
   buttonPlus.addEventListener('click', function() {
-    openPopup(popup);
+    openPopup(popupMesto);
   })
   buttonClose.addEventListener('click', function() {
-    closePopup(popup);
+    console.log('hey');
+    //closePopup(popup);
   })
-  buttonToCloseImg.addEventListener('click', function() {
-    closePopup(popup);
-  });
 
 }
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,47 @@
-const popup = document.querySelector('.popup');
-const buttonEdit = document.querySelector('.profile__button-edit');
-const closeIcon = document.querySelector('.popup__close-icon');
+const selectors = {
+  popup: '.popup',
+  popupProfile: '.popup-profile',
+  popupMesto: '.popup-mesto',
+  popupImage: '.popup-image',
+  buttonClose: '.popup__close-icon',
+  buttonEdit: '.profile__button-edit',
+  form: '.popup-mesto__form',
+  inputName: '.popup-mesto__input_type_name',
+  inputLink: '.popup-mesto__input_type_link',
+  list: '.elements',
+  buttonPlus: '.profile__button',
+  template: '.template-card',
+  divElementsCard: '.elements__card',
+  elementsTitle: '.elements__title',
+  elementsImage: '.elements__image',
+  trashButton: '.elements__trash',
+  wrapperButton: '.elements__button-wrapper',
+  popopImageImage: '.popup-image__image',
+  caption: '.popup-image__caption',
+  like: '.elements__like',
+}
+
 const formPopup = document.querySelector('.popup__form');
 const profileName = document.querySelector('.profile__name');
 const popupProfileInputName = document.querySelector('.popup__input_type_name');
 const profileJob = document.querySelector('.profile__busy');
 const popupProfileInputJob = document.querySelector('.popup__input_type_job');
+
+const popup = document.querySelector(selectors.popup);
+const popupProfile = document.querySelector(selectors.popupProfile);
+const popupImage = document.querySelector(selectors.popupImage);
+const form = document.querySelector(selectors.form);
+const inputName = form.querySelector(selectors.inputName);
+const inputLink = form.querySelector(selectors.inputLink);
+const list = document.querySelector(selectors.list);
+const buttonPlus = document.querySelector(selectors.buttonPlus);
+const popupMesto = document.querySelector(selectors.popupMesto);
+const buttonClose = document.querySelector(selectors.buttonClose);
+const popopImageImage = popupImage.querySelector(selectors.popopImageImage);
+const caption = popupImage.querySelector(selectors.caption);
+const buttonEdit = document.querySelector(selectors.buttonEdit);
+const template = document.querySelector(selectors.template).content.querySelector(selectors.divElementsCard);
+
 
 function closePopup(p1) {
   p1.classList.remove('popup_opened');
@@ -27,7 +63,7 @@ function EditProfileForm () {
 
 buttonEdit.addEventListener('click', EditProfileForm);
 formPopup.addEventListener('submit', handleProfileFormSubmit);
-closeIcon.addEventListener('click', () => closePopup(popup));
+//buttonClose.addEventListener('click', () => closePopup(popup));
 
 
 
