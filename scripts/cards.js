1210e6c9c1d6333ca060b190349c5a2b25ef4612
TimAbuev <tim-r10@yandex.ru>
2022-08-05 22:55:48 +0300

    добавить атрибут alt в скрипте

--- scripts/cards.js
+++ scripts/cards.js
@@ -3,11 +3,12 @@ function createCard(name, imageSrc) {
   const elementsTitle = newTemplate.querySelector(selectors.elementsTitle);
   elementsTitle.textContent = name;
   newTemplate.querySelector(selectors.elementsImage).setAttribute('src', imageSrc);
-  newTemplate.querySelector(selectors.elementsImage).setAttribute('alt', 'имя картинки вставлять');
+  newTemplate.querySelector(selectors.elementsImage).setAttribute('alt', name);
   newTemplate.querySelector(selectors.trashButton).addEventListener('click', function() {newTemplate.remove();})
   newTemplate.querySelector(selectors.wrapperButton).addEventListener('click', function() {
     openPopup(popupImage);
     popopImageImage.setAttribute('src', imageSrc);
+    popopImageImage.setAttribute('alt', name);
     caption.textContent = name;
   });
   newTemplate.querySelector(selectors.like).addEventListener('click', function() {
