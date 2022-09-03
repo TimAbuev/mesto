1946f1505cbe17b1f5bb1ef22c9403bb874fd3a3
TimAbuev <tim-r10@yandex.ru>
2022-09-03 17:47:04 +0300

    добавить метод _generate

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,9 +1,33 @@
+class Card {
+  constructor(data, template) {
+    this._text = data.name;
+    this._image = data.link;
+    this._template = template;
+  }
+  _getElement() {
+    const newTemplate = document.querySelector(this._template).cloneNode(true);
+    return newTemplate;
+    
+    //const elementsTitle = newTemplate.querySelector(selectors.elementsTitle);
+    //elementsTitle.textContent = name;
+    //newTemplate.querySelector(selectors.elementsImage).setAttribute('src', imageSrc);
+    //newTemplate.querySelector(selectors.elementsImage).setAttribute('alt', name);
+    
+  }
+  _generate() {
+    this._element = this._getElement();
+    this._element.querySelector(selectors.elementsImage).setAttribute('src', this._image);
+    this._element.querySelector(selectors.elementsImage).setAttribute('alt', this._text);
+    const elementsTitle = this._element.querySelector(selectors.elementsTitle);
+    elementsTitle.textContent = this._text;
+  }
+  _setEventListeners() {
+    
+  }
+}
+
 function createCard(name, imageSrc) {
-  const newTemplate = template.cloneNode(true);
-  const elementsTitle = newTemplate.querySelector(selectors.elementsTitle);
-  elementsTitle.textContent = name;
-  newTemplate.querySelector(selectors.elementsImage).setAttribute('src', imageSrc);
-  newTemplate.querySelector(selectors.elementsImage).setAttribute('alt', name);
+
   newTemplate.querySelector(selectors.trashButton).addEventListener('click', function() {newTemplate.remove();})
   newTemplate.querySelector(selectors.wrapperButton).addEventListener('click', function() {
     openPopup(popupImage);
@@ -16,6 +40,7 @@ function createCard(name, imageSrc) {
   });
   return newTemplate;
 }
+
 
 function addCard(name, link) {
   const newCard = createCard(name, link);
